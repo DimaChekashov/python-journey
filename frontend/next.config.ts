b5d3d35cb2ccae49058d6a4c60ca539c8fb13e3e
DimaChekashov <foxdima99@gmail.com>
2025-06-14 15:21:32 +0300

    Prepare basic structure

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  pageExtensions: ["page.jsx", "page.tsx", "page.js", "page.jsx"],
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
