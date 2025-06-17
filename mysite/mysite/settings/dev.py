a00932cfe02a03e50ccc12fa10828383d0e33885
DimaChekashov <foxdima99@gmail.com>
2025-06-17 17:53:28 +0300

    Add wagtail cms

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-auy$5utgy@h4(e5(6vj^7a+sk+r7&2di2d(gqj9$-^fro=&iac"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
