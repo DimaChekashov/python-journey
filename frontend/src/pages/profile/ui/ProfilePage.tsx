2c67a5a2df95498e3835c6d179cd41cab1eaed06
DimaChekashov <foxdima99@gmail.com>
2025-06-20 23:11:49 +0300

    add Profile page

---
+++ frontend/src/pages/profile/ui/ProfilePage.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Profile() {
+  return (
+    <>
+      <Head>
+        <title>Profile: Wagtail + NextJS</title>
+      </Head>
+      <div>
+        <h1>Profile Page</h1>
+      </div>
+    </>
+  );
+}
