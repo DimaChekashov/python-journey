0ca57563fe8dadef6e07c3cf15ff2566abb4a838
DimaChekashov <foxdima99@gmail.com>
2025-06-22 12:19:50 +0300

    add get post on frontend from django

--- frontend/pages/posts/[id].tsx
+++ frontend/pages/posts/[id].tsx
@@ -1,6 +1,6 @@
 import { GetServerSidePropsContext } from "next";
 import { PostPage } from "@/pages/post";
-import posts from "@/shared/const/posts.json";
+// import posts from "@/shared/const/posts.json";
 import { Post as PostType } from "@/shared/types/Post";
 import PostProvider from "@/app/providers/Post/PostProvider";
 import { checkSSRRateLimit } from "@/shared/lib/rateLimiter";
@@ -13,9 +13,11 @@ export async function getServerSideProps(
 
   const { id } = context.params!;
 
-  const post = posts.posts.find((post) => post.id === id);
+  // const post = posts.posts.find((post) => post.id === id);
+  const res = await fetch(`http://localhost:8000/api/posts/${id}/`);
+  const post = await res.json();
 
-  if (!post) {
+  if (!post || post.detail) {
     return { notFound: true };
   }
 
