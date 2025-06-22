612bb07827ce6091c266ae793b8bcec30b28df7f
DimaChekashov <foxdima99@gmail.com>
2025-06-22 14:12:32 +0300

    add fetching posts data for PostsPage

--- frontend/pages/posts/index.tsx
+++ frontend/pages/posts/index.tsx
@@ -1,5 +1,20 @@
 import { PostsPage } from "@/pages/posts";
+import { Post as PostType } from "@/shared/types/Post";
+import { GetServerSidePropsContext } from "next";
+import { checkSSRRateLimit } from "@/shared/lib/rateLimiter";
 
-export default function Post() {
-  return <PostsPage />;
+export async function getServerSideProps(
+  context: GetServerSidePropsContext<{ id: string }>
+) {
+  const rateLimitResult = await checkSSRRateLimit(context.req);
+  if (rateLimitResult.redirect) return rateLimitResult;
+
+  const res = await fetch(`http://localhost:8000/api/posts/`);
+  const posts = await res.json();
+
+  return { props: { posts } };
+}
+
+export default function Post({ posts }: { posts: PostType[] }) {
+  return <PostsPage posts={posts} />;
 }
