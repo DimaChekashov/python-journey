cf05c0ec80203381c5fb6f24c82e6ca9d1fe762a
DimaChekashov <foxdima99@gmail.com>
2025-06-23 23:56:33 +0300

    Pass data into templates

--- learndjango/main/views.py
+++ learndjango/main/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Main page!!',
+        'values': ['Hello', 'Some', "World"],
+        'obj': {
+            'car': 'BMW',
+            'age': 10,
+            'hobby': 'Football'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
