b757139ca423c578a07858645c585fb97d8e205c
DimaChekashov <foxdima99@gmail.com>
2025-06-23 09:37:50 +0300

    add html templates

--- learndjango/main/views.py
+++ learndjango/main/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
-    return HttpResponse("<h4>Check working...</h4>")
+    return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse("<h4>About page</h4>")
-
-def profile(request):
-    return HttpResponse("<h4>Profile page</h4>")
+    return render(request, 'main/about.html')
