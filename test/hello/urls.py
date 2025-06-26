ce98266f15773c94a5bb8dfe2ca1851f0396a129
DimaChekashov <foxdima99@gmail.com>
2025-06-26 08:07:26 +0300

    add re path

--- test/hello/urls.py
+++ test/hello/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name="home"),
-    path('some-url', views.some_url, name="some_url"),
+    re_path(r'^some-url', views.some_url, name="some_url"),
     path('calc', views.calc, name="calc"),
+    path('about', views.about, name="about", kwargs={"name":"Tom", "age": 38})
 ]
--- test/hello/views.py
+++ test/hello/views.py
@@ -11,3 +11,10 @@ def calc(request):
     a = 10
     b = 20
     return HttpResponse(f"{a} + {b} = {a + b}")
+
+def about(request, name, age):
+    return HttpResponse(f"""
+        <h2>О пользователе</h2>
+        <p>Имя: {name}</p>
+        <p>Возраст: {age}</p>                    
+    """)
