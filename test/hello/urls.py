1752d649dbb92ba034c54df412836addc12e7490
DimaChekashov <foxdima99@gmail.com>
2025-06-26 07:54:32 +0300

    add urls

--- test/hello/urls.py
+++ test/hello/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="home"),
+    path('some-url', views.some_url, name="some_url"),
+    path('calc', views.calc, name="calc"),
 ]
--- test/hello/views.py
+++ test/hello/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello")
+
+def some_url(request):
+    return HttpResponse("Some url")
+
+def calc(request):
+    a = 10
+    b = 20
+    return HttpResponse(f"{a} + {b} = {a + b}")
