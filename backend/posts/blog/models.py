5410a31ddb793a457048da1d495df20fb3fa650a
DimaChekashov <foxdima99@gmail.com>
2025-08-09 17:50:44 +0300

    clear backend

--- backend/posts/blog/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    author_id = models.CharField(max_length=36)
